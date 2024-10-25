d10124256c4bf4d462c94b7c9df1aa71638a5c16
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-25 09:22:55 +0530

    Update welcome.py

--- SUHANIMUSIC/plugins/tools/welcome.py
+++ SUHANIMUSIC/plugins/tools/welcome.py
@@ -82,7 +82,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "AarohiX/assets/wel2.png"
+        pic = "SUHANIMUSIC/assets/wel2.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
