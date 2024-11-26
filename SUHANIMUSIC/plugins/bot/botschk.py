75aa52d485a91b973b3f51e997ec8f9431925b84
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-11-26 21:02:59 +0530

    Update botschk.py

--- SUHANIMUSIC/plugins/bot/botschk.py
+++ SUHANIMUSIC/plugins/bot/botschk.py
@@ -26,11 +26,11 @@ from datetime import datetime
 userbot = Userbot()
 
 
-BOT_LIST = ["Venom_X_Music_bot", "Suhani_X_Music_bot"]
+BOT_LIST = ["TheAnjaliMusicBot", "TheAnjaliRobot"]
 
 @app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
-    msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
+    msg = await message.reply_photo(photo="https://envs.sh/qii.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
     for bot_username in BOT_LIST:
         try:
