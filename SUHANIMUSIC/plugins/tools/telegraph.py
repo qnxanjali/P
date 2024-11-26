13ab3907607cbcc312fd911c26efd25f2f886320
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-11-26 22:51:40 +0530

    Update telegraph.py

--- SUHANIMUSIC/plugins/tools/telegraph.py
+++ SUHANIMUSIC/plugins/tools/telegraph.py
@@ -2,7 +2,7 @@ import logging
 import os
 from pyrogram import filters
 from pyrogram.types import Message
-from SafoneAPI import api
+from TheAPI import api
 from SUHANIMUSIC import app
 
 # Setup logging
